c45e90a60a106c27a5e1aa6bdf205551edee92d7
vsreyes <virgilio.s.reyes@gmail.com>
2020-11-14 12:27:43 +0100

    Fontawesome on Navbar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Header = () => {
   return (
     <header>
-      <Container>
-        <Row>
-          <Col className='text-center py-3'>Copyright &copy; Proshop</Col>
-        </Row>
-      </Container>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href='/'>ProShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <Nav.Link href='/cart'>
+                <i className='fas fa-shopping-cart'></i> Cart
+              </Nav.Link>
+              <Nav.Link href='/login'>
+                <i className='fas fa-user'></i> Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
   );
 };
