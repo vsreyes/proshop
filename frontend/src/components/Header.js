58029235824a7fc0f8f0b4ca612c6591de04434b
vsreyes <virgilio.s.reyes@gmail.com>
2020-11-14 11:47:33 +0100

    Header and Footer edit

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Header = () => {
-  return <header>header</header>;
+  return (
+    <header>
+      <Container>
+        <Row>
+          <Col className='text-center py-3'>Copyright &copy; Proshop</Col>
+        </Row>
+      </Container>
+    </header>
+  );
 };
 
 export default Header;
