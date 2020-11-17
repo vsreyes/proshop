50dc29757b43fba4916a1fe17ac46592b11fc738
vsreyes <virgilio.s.reyes@gmail.com>
2020-11-17 14:39:55 +0100

    Product Info

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import Rating from '../components/Rating';
+import products from '../products';
 
-const ProductScreen = () => {
-  return <div>Product</div>;
+const ProductScreen = ({ match }) => {
+  const product = products.find(p => p._id === matchMedia.params.id);
+
+  return <div>{product.name}</div>;
 };
 
 export default ProductScreen;
