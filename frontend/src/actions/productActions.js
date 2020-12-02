efe735893bda091bc297b1821b5475dc724d6f98
vsreyes <virgilio.s.reyes@gmail.com>
2020-12-02 21:54:14 +0100

    Productscreen corrections

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -29,7 +29,7 @@ export const listProducts = () => async dispatch => {
   }
 };
 
-export const listProductDetails = () => async dispatch => {
+export const listProductDetails = id => async dispatch => {
   try {
     dispatch({ type: PRODUCT_DETAILS_REQUEST });
 
