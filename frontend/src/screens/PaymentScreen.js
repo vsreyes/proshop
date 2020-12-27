032ddc3afc3f00f279c3aa6cb9d55889165a4efe
vsreyes <virgilio.s.reyes@gmail.com>
2020-12-27 14:46:45 +0100

    Order setup

--- frontend/src/screens/PaymentScreen.js
+++ frontend/src/screens/PaymentScreen.js
@@ -33,7 +33,7 @@ const PaymentScreen = ({ history }) => {
           <Col>
             <Form.Check
               type='radio'
-              label='Paypal or Credit Card'
+              label='PayPal or Credit Card'
               id='PayPal'
               name='paymentMethod'
               value='PayPal'
--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -20,7 +20,7 @@ const PlaceOrderScreen = ({ history }) => {
     cart.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
   );
   cart.shippingPrice = addDecimals(cart.itemsPrice > 100 ? 0 : 100);
-  cart.taxPrice = addDecimals(Number((0.25 * cart.itemsPrice).toFixed(2)));
+  cart.taxPrice = addDecimals(Number((0.15 * cart.itemsPrice).toFixed(2)));
   cart.totalPrice = (
     Number(cart.itemsPrice) +
     Number(cart.shippingPrice) +
