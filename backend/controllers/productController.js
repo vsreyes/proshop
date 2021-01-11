6d5ec330b185301a9b4c3bd1b6a9f88f09f4ce8e
vsreyes <virgilio.s.reyes@gmail.com>
2021-01-11 13:03:07 +0100

    Update product setup

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -39,4 +39,50 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    Create a product
+// @route   DELETE /api/products
+// @access  Private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: 'Sample name',
+    price: 0,
+    user: req.user._id,
+    image: '/images/sample.jpg',
+    brand: 'Sample brand',
+    category: 'Sample category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'Sample description',
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(product);
+});
+
+// @desc    Update a product
+// @route   PUT /api/products/:id
+// @access  Private/Admin
+const updateProduct = asyncHandler(async (req, res) => {
+  const {
+    name,
+    price,
+    description,
+    image,
+    brand,
+    category,
+    countInStock,
+  } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+
+  const createdProduct = await product.save();
+  res.status(201).json(product);
+});
+
 export { getProducts, getProductById, deleteProduct };
