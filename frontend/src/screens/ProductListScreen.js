4e386a66b7effe7fe78af928a905c49d43ce34de
vsreyes <virgilio.s.reyes@gmail.com>
2021-01-18 13:55:13 +0100

    Correction on create new product button

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -73,7 +73,7 @@ const ProductListScreen = ({ history, match }) => {
           <h1>Products</h1>
         </Col>
         <Col className='text-right'>
-          <Button className='my-3' onCLick={createProductHandler}>
+          <Button className='my-3' onClick={createProductHandler}>
             <i className='fas fa-plus'></i> Create Product
           </Button>
         </Col>
